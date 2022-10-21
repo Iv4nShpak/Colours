562006802d1174738c1412f9c68f670744e7688a
IShpak <84473401+Iv4nShpak@users.noreply.github.com>
2022-10-21 08:21:17 +0300

    add elements

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const cols = document.querySelectorAll('.col')
 
-function randomColor(){
+// function randomColor(){
 
-    const hexCodes = '0123456789ABCDEF'
-    let color = ''
-    for (let i = 0; i < 6; i++){
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-    }
-    return '#' + color
-}
+//     const hexCodes = '0123456789ABCDEF'
+//     let color = ''
+//     for (let i = 0; i < 6; i++){
+//         color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+//     }
+//     return '#' + color
+// }
 
 function setRandomColors(){
     cols.forEach((col) => {
-        col.style.background = randomColor()
+        const text = col.querySelector('h2')
+        const color = chroma.random()
+
+        text.textContent = color
+        col.style.background = color
+
+        setTextColor(text, color)
     })
+}
+
+function setTextColor(text, color){
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 setRandomColors()
